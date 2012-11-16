5d48bf7c54a19eb8f53bf5541bc188651527b5ef
James Hibberd <james.d.hibberd@gmail.com>
2012-11-15 22:08:03 -0800

    Added binary groupings as model for vectors.

--- titanic/Main.hs
+++ titanic/Main.hs
@@ -1,95 +1,170 @@
+{-
+TODO:
+-- Add fareGroup
+-- Once all weights have been established pick the strongest n and run again
+   with just those weights. Might avoid overfitting. Does this improve score?
+-}
 
 import Text.CSV
 import Debug.Trace
 import Data.List
 
-trainingFilename = "/home/jhibberd/projects/learning/titanic/train.csv"
-testFilename = "/home/jhibberd/projects/learning/titanic/test.csv"
+-- Config ----------------------------------------------------------------------
 
-threshold = 0.449
-bias = 0.0
-learningRate = 0.0005
+trainingFilename =      "/home/jhibberd/projects/learning/titanic/train.csv"
+testFilename =          "/home/jhibberd/projects/learning/titanic/test.csv"
+learningRate =          0.0005
+trainingIterations =    500
+
 
 type Filename = String
 
 main = do
 
-    --putStrLn "Reading training data from file..."
+    putStrLn "Reading training data from file..."
     dataset <- readCSV trainingFilename True
-    --putStrLn ("Read " ++ show (length dataset) ++ " records.")
+    putStrLn ("Read " ++ show (length dataset) ++ " records.")
 
-    --putStrLn "Extracting desired outputs..."
+    putStrLn "Extracting desired outputs..."
     let outputs = toOutputs dataset
         dataset' = removeOutputs dataset
 
-    --putStrLn "Converting dataset to vectors..."
+    putStrLn "Converting dataset to vectors..."
     let vectorMatrix = toVectorMatrix dataset'
 
-    --putStrLn ("Generated " ++ show (length vectorMatrix) ++ " vector series.")
-    --putStrLn "Combining vector series..."
+    putStrLn ("Generated " ++ show (length vectorMatrix) ++ " vector series.")
+    putStrLn "Combining vector series..."
     let combined = combineVectors vectorMatrix
     let combined' = zip combined outputs
 
-    --putStrLn "Learning correlation weights..."
+    putStrLn "Learning correlation weights..."
     let weights = initWeights combined
-    let weights' = train weights combined'
-    --putStrLn "Learnt weights:"
-    --putStrLn $ show weights'
+    let (weights', error) = train weights combined' ([], 1) trainingIterations
+    putStrLn ("Learnt weights with error " ++ show error ++ ":")
+    pprintWeights weights'
 
-
-    --putStrLn "Reading test data from file..."
+    putStrLn "Reading test data from file..."
     testset <- readCSV testFilename True
-    --putStrLn ("Read " ++ show (length testset) ++ " records.")
+    putStrLn ("Read " ++ show (length testset) ++ " records.")
 
     -- Quick n dirty
-
+    {-
     let vectorMatrix' = toVectorMatrix testset
         vectorMatrix'' = combineVectors vectorMatrix'
     let cs = map (classify weights') vectorMatrix''
         cs' = map (show . floor) cs
     prependFile testFilename ("survived":cs')
-
-
-    {-
-    -- Learn weights
-    trainingCSV <- parseCSVFromFile trainingFilename
-    let dataSet = getDataSet trainingCSV
-        trainingOutcomes = getOutcomesFromDataSet dataSet
-        trainingItems = getItemsFromDataSet dataSet
-        trainingSet = zip (vectoriseItems trainingItems) trainingOutcomes
-        learntWeights = train (initWeights trainingItems) trainingSet
-
-    let learntWeights' = traceShow learntWeights learntWeights
-
-    -- Apply learnt weights to test set
-    testCSV <- parseCSVFromFile testFilename
-    let testSet = getTestSet testCSV
-
-    let cs = map (classify learntWeights') (vectoriseItems testSet)
-        cs' = map (show . floor) cs
-    prependFile testFilename ("survived":cs')
     -}
 
-toVectorMatrix :: [[String]] -> [[Float]] 
-toVectorMatrix ds = [
+pprintWeights :: [Float] -> IO ()
+pprintWeights ws = mapM_ f (zip vectorLabels ws)
+    where f (lbl, w) = putStrLn ("\t" ++ lbl ++ "\t" ++ show w)
 
-    toVectors ds 2 (\x -> if x == "female" then 1 else 0),
-    toVectors ds 2 (\x -> if x == "male" then 1 else 0),
-
-    toVectors ds 0 (\x -> if x == "1" then 1 else 0),
-    toVectors ds 0 (\x -> if x == "2" then 1 else 0),
-    toVectors ds 0 (\x -> if x == "3" then 1 else 0),
-
-    toVectors ds 3 (isAgeGroup 1),
-    toVectors ds 3 (isAgeGroup 2),
-    toVectors ds 3 (isAgeGroup 3),
-    toVectors ds 3 (isAgeGroup 4),
-    toVectors ds 3 (isAgeGroup 5)
-
+-- | Human friendly labels explaining what each vector represents.
+--
+-- Used to pretty printing the learnt weights.
+vectorLabels :: [String]
+vectorLabels = [
+    "Is female..................",
+    "Is male....................",
+    "Is first class.............",
+    "Is second class............",
+    "Is third class.............",
+    "Is aged <18................",
+    "Is aged 18-29..............",
+    "Is aged 30-39..............",
+    "Is aged 40-49..............",
+    "Is aged 50+................",
+    "Did embark at 'C'..........",
+    "Did embark at 'Q'..........",
+    "Did embark at Southampton..",
+    "In cabin tier A............",
+    "In cabin tier B............",
+    "In cabin tier C............",
+    "In cabin tier D............",
+    "In cabin tier E............",
+    "In cabin tier F............",
+    "In cabin tier G............",
+    "In cabin tier H............"
+    "Has no siblings............",
+    "Has 1 sibling..............",
+    "Has 2 siblings.............",
+    "Has 3 siblings.............",
+    "Has 4+ siblings............",
+    "Has no parents.............",
+    "Has 1 parent...............",
+    "Has 2 parents..............",
+    "Has 3 parents..............",
+    "Has 4 parents.............."
     ]
 
-isAgeGroup :: Float -> String -> Float
-isAgeGroup g a = if ageGroup a == g then 1 else 0
+-- | Transform raw, string-based training rows to binary vectors.
+--
+-- Each value in the binary vector represents whether the row belongs to a 
+-- particular set (0/no, 1/yes).
+toVectorMatrix :: [[String]]    -- String-based dataset to convert
+               -> [[Float]]     -- List of vectors
+toVectorMatrix d = [
+
+    -- Gender
+    toVectors d 2 (\x -> b (x == "female")),
+    toVectors d 2 (\x -> b (x == "male")),
+
+    -- Class
+    toVectors d 0 (\x -> b (x == "1")),
+    toVectors d 0 (\x -> b (x == "2")),
+    toVectors d 0 (\x -> b (x == "3")),
+    
+    -- Age
+    toVectors d 3 (\x -> b ((ageGroup x) == 1)),
+    toVectors d 3 (\x -> b ((ageGroup x) == 2)),
+    toVectors d 3 (\x -> b ((ageGroup x) == 3)),
+    toVectors d 3 (\x -> b ((ageGroup x) == 4)),
+    toVectors d 3 (\x -> b ((ageGroup x) == 5)),
+
+    -- Embarked
+    toVectors d 9 (\x -> b (x == "C")),
+    toVectors d 9 (\x -> b (x == "Q")),
+    toVectors d 9 (\x -> b (x == "S")),
+
+    -- Cabin group
+    toVectors d 8 (\x -> b ((cabinGroup x) == 1)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 2)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 3)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 4)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 5)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 6)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 7)),
+    toVectors d 8 (\x -> b ((cabinGroup x) == 8))
+
+    -- Siblings
+    toVectors d 4 (\x -> b ((siblingGroup x) == 1)),
+    toVectors d 4 (\x -> b ((siblingGroup x) == 2)),
+    toVectors d 4 (\x -> b ((siblingGroup x) == 3)),
+    toVectors d 4 (\x -> b ((siblingGroup x) == 4)),
+    toVectors d 4 (\x -> b ((siblingGroup x) == 5)),
+
+    -- Parents
+    toVectors d 5 (\x -> b (x == "0")),
+    toVectors d 5 (\x -> b (x == "1")),
+    toVectors d 5 (\x -> b (x == "2")),
+    toVectors d 5 (\x -> b (x == "3")),
+    toVectors d 5 (\x -> b (x == "4"))
+
+    ]
+    where b True =  1
+          b False = 0
+
+
+-- Vector transformations ------------------------------------------------------
+
+siblingGroup :: String -> Float
+siblingGroup "" =   0
+siblingGroup "0" =  1
+siblingGroup "1" =  2
+siblingGroup "2" =  3
+siblingGroup "3" =  4 
+siblingGroup _ =    5 
 
 ageGroup :: String -> Float
 ageGroup "" = 0
@@ -100,6 +175,23 @@ ageGroup x = group $ (read x :: Float)
               | n < 40 = 3
               | n < 50 = 4
               | otherwise = 5
+
+cabinGroup :: String -> Float
+cabinGroup "" = 0
+cabinGroup x = letterScore cabin
+    where cabin = (words x) !! 0
+          letterScore c = case (c !! 0) of
+                              'A' -> 1
+                              'B' -> 2
+                              'C' -> 3
+                              'D' -> 4
+                              'E' -> 5
+                              'F' -> 6
+                              'G' -> 7
+                              'T' -> 8
+
+
+-- Helpers ---------------------------------------------------------------------
 
 -- | Combines multiple vector lists.
 --
@@ -139,7 +231,9 @@ toVectors ds i f = map f onlyCol
 --
 -- If 'headless' is set to true then the first line containing the column
 -- names is ignored. Any trailing empty line is ignored.
-readCSV :: Filename -> Bool -> IO [[String]]
+readCSV :: Filename 
+        -> Bool 
+        -> IO [[String]]
 readCSV fn headless = do
     content <- parseCSVFromFile fn
     case content of
@@ -148,33 +242,53 @@ readCSV fn headless = do
                      return $ if headless then tail a' else a'
     where removeBlankLines = filter (/= [""])
 
+prependFile :: FilePath 
+            -> [String] 
+            -> IO ()
+prependFile fn xs = do
+    content <- readFile fn
+    let content' = lines content
+    let result = map (\(x, y) -> x ++ "," ++ y) $ zip xs content'
+    mapM_ putStrLn result
 
--- Perceptron Algorithm --------------------------------------------------------
+type Sample = ([Float], Float)
+type TrainingSet = [Sample]
+
+
+-- Perceptron algorithm --------------------------------------------------------
 
 -- | Return a list of equal size to the input vectors containing zeros.
-initWeights :: [[Float]] -> Weights
+initWeights :: [[Float]]    -- All input vectors
+            -> [Float]      -- Returns zero'd list of weights of equal length
+                            -- to input vectors
 initWeights (x:_) = take (length x) $ repeat 0.0
 
-{-
--- Original 'train' function whos exit condition was an error sum below a
--- constant threshold
-train :: Weights -> TrainingSet -> Weights
-train ws xs = let (ws', errorSum) = iterate' ws xs 0
-              in if errorSum / n <= 0-- threshold
-                  then ws'
-                  else train ws' (traceShow (errorSum / n)  xs)
-    where n = fromIntegral $ length xs
--}
+-- | May not produce the best outcome. Maybe pick lowest error score after
+-- n iterations?
+train :: [Float]            -- Initial or current weights 
+      -> TrainingSet 
+      -> ([Float], Float)   -- Best error score achieved and corresponding 
+                            -- weights
+      -> Float              -- Iterations remaining
+      -> ([Float], Float)   -- Return best weights and error
+train _ _ (bWeights, bError) 0 = (bWeights, bError)
+train ws xs (bWeights, bError) n = 
 
-train :: Weights -> TrainingSet -> Weights
-train ws xs = let (ws', errorSum) = iterate' ws xs 0
-                  errorSum' = errorSum / n
-              in if ws == ws' -- End when weights-in == weights following iteration. Otherwise infinite loop.
-                  then ws'
-                  else train ws' xs
-    where n = fromIntegral $ length xs
+    let (ws', errorSum) = iterate' ws xs 0
+        error = errorSum / numTrainingVectors
 
-iterate' :: Weights -> TrainingSet -> ErrorSum -> (Weights, ErrorSum)
+        newBest = if error < bError
+            then (ws', error)
+            else (bWeights, bError)
+
+    in train ws' xs newBest (n-1)
+    where numTrainingVectors = fromIntegral $ length xs
+
+
+iterate' :: [Float] 
+         -> TrainingSet 
+         -> Float               -- Error 
+         -> ([Float], Float)    -- Weights and error
 iterate' ws [] e = (ws, e)
 iterate' ws ((x, expected):xs) errorSum = 
     let actual = classify ws x
@@ -183,184 +297,22 @@ iterate' ws ((x, expected):xs) errorSum =
         ws' = adjustWeights ws x error
     in iterate' ws' xs errorSum'
 
-classify :: Weights -> InputVector -> Classification
+-- | Given weights, classify a vector.
+classify :: [Float]         -- Weights
+         -> [Float]         -- Vector 
+         -> Float           -- Classification (1 or 0)
 classify ws xs = bin $ sum [w * x | (w, x) <- zip ws xs]
-    where bin x | x > 0 = 1
-          bin x | otherwise = 0
+    where bin x 
+              | x > 0 = 1
+              | otherwise = 0
 
-adjustWeights :: Weights -> InputVector -> Error -> Weights
+-- | Given weights, a vector and the error that resulted from classifying that
+-- vector with those weights, adjust the weights so as to reduce the chance
+-- of future misclassification.
+adjustWeights :: [Float]        -- Weights
+              -> [Float]        -- Vector
+              -> Float          -- Error
+              -> [Float]        -- Adjusted weights
 adjustWeights ws _ 0 = ws
 adjustWeights ws xs e = [w + (learningRate * e * x) | (w, x) <- zip ws xs]
-
-
-
--- OLD -------------------------------------------------------------------------
-
--- Usage: getStrongestSeq [1, (-6), 2, 3, 4, 5] (permutations [1, 2, 3])
-
--- | Given a list of weights and a list of sequences that those weights
--- correspond to, the function will return a pair containing the largest
--- absolute weight and its associated sequence.
-getStrongestSeq :: [Float] -> [[a]] -> ([a], Float)
-getStrongestSeq ns xs = let (i, x) = topEl in (xs !! i, x) 
-    where topEl = foldr combiningFunc (0, 0) indexed
-          indexed = zip [0..] ns -- indexed elements in 'ns' list
-          combiningFunc (i, x) (topI, topX) 
-              | abs x > topX = (i, abs x)
-              | otherwise = (topI, topX)
-
-{-
-getStrongestPermutation :: IO ()
-getStrongestPermutation = do
-
-    trainingCSV <- parseCSVFromFile trainingFilename
-    let dataSet = getDataSet trainingCSV
-        trainingOutcomes = getOutcomesFromDataSet dataSet
-        trainingItems = getItemsFromDataSet dataSet
-        trainingSet = zip (vectoriseItems trainingItems) trainingOutcomes
-        learntWeights = train (initWeights trainingItems) trainingSet
-        max = maximum $ map abs learntWeights
-        i = elemIndex
-    return 3
--}
-
--- | Return the index of the element with the absolute max value.
---absMaxIndex :: [Float] -> Int
---absMaxIndex xs = foldr (\(i, x), (biggestI, biggestX) -> if abs x > biggestX then (i, abs x) else (biggestI, biggestX)) 0 indexedXs
---    where indexedXs = zip [0...] xs
-
-getOutcomesFromDataSet :: [[String]] -> [Float]
-getOutcomesFromDataSet = map (asBinary . head)
-    where asBinary "0" = 0
-          asBinary "1" = 1
-
-getItemsFromDataSet :: [[String]] -> [[String]]
-getItemsFromDataSet = map tail
-
---vectoriseItems :: [[String]] -> [[Float]]
---vectoriseItems = map toVector
-
--- | Read the test file as a list of lists.
---getTestSet :: Either a [[String]] -> [[String]]
---getTestSet (Left a) = error "Can't read test file."
---getTestSet (Right a) = tail $ init a
-
-prependFile :: FilePath -> [String] -> IO ()
-prependFile fn xs = do
-    content <- readFile fn
-    let content' = lines content
-    let result = map (\(x, y) -> x ++ "," ++ y) $ zip xs content'
-    mapM_ putStrLn result
-
-type Weights = [Float]
-type Sample = (InputVector, Float)
-type TrainingSet = [Sample]
-type InputVector = [Float]
-type Error = Float
-type ErrorSum = Float
-type Classification = Float
-
--- TODO(jhibberd) Independent function to test which permutation has strongest
--- correlation with survival rate. Use that permutation only. Suspect too many
--- weights causes watering down of results where each outcome is barely above
--- or below the threshold.
-
--- | To vector -----------------------------------------------------------------
-
--- | Convert an items of string types to a vector of float values.
---
--- TODO(jhibberd) List all fields in items.
--- TODO(jhibberd) Just don't include mapping for those that can't be 
--- interpreted.
--- TODO(jhibberd) Max 5 permutations, so formatter which converts actual value
--- to a category that appears in the permutation
---toVector :: [String] -> [Float]
---toVector xs = map (\(i, f) -> f (xs !! i)) converters
---    where converters = (permMap 3 ageGroup ("":(map show [1..5])))
-    {-
-    where converters = [ mapToFloat ["1", "2", "3"] id ] ++
-                       [(1, quantifyName)] ++
-                       (permMap 2 id ["male", "female"]) ++ 
-                       (permMap 3 ageGroup ("":(map show [1..5]))) ++
-                       [ (3, quantifyAge) ] ++
-                       (permMap 4 sibsp (map show [0..4])) ++
-                       (permMap 5 sibsp (map show [0..4])) ++
-                       [
-                         (6, quantifyTicket)
-                       , (7, quantifyFare)
-                       , (8, quantifyCabin)
-                       , (9, quantifyEmbarked)
-                       ]
-    -}
-
--- Apparently gender makes no difference.
-
-{-
-ageGroup :: String -> String
-ageGroup "" = ""
-ageGroup x = show $ ((floor ((read x) / 20)) + 1)
--}
-sibsp :: String -> String
-sibsp x
-    | (read x) > 3 = "4"
-    | otherwise = x
-
-permMap :: Int -> (String -> String) -> [String] -> [(Int, (String -> Float))]
-permMap i f m = map (\x -> (i, mapToFloat x f)) $ permutations m
-
-mapToFloat :: [String] -> (String -> String) -> String -> Float
-mapToFloat xs f x = case elemIndex (f x) xs of
-                        Just i -> fromIntegral (i+1)
-                        Nothing -> error "Parse error"
-
--- | Virtually impossible to quantify name into a meaninful number.
-quantifyName :: String -> Float
-quantifyName x = 0
-
-quantifyAge :: String -> Float
-quantifyAge "" = 0
-quantifyAge x = read x
-
-quantifySibsp :: String -> Float
-quantifySibsp = read
-
-quantifyParch :: String -> Float
-quantifyParch = read
-
--- | Ignore for now. It may be that the ticket number can be deconstructed to
--- give an indication of where on the ship the passenger was most likely to
--- have been at the time of the accident.
-quantifyTicket :: String -> Float
-quantifyTicket x = 0
-
-quantifyFare :: String -> Float
-quantifyFare "" = 0
-quantifyFare x = read x
-
--- | TODO(jhibberd) Try "dynamic fields" with lists ordered in multiple ways
-
--- | Ignore for now. See 'quantifyTicket'.
-quantifyCabin :: String -> Float
-quantifyCabin "" = 0
-quantifyCabin x = letterScore cabin * number tail'
-    where cabin = (words x) !! 0
-          tail' = tail cabin
-          number n 
-              | n == "" = 0
-              | otherwise = read n / 100
-          letterScore c = case (c !! 0) of
-                              'A' -> 1
-                              'B' -> 2
-                              'C' -> 3
-                              'D' -> 4
-                              'E' -> 5
-                              'F' -> 6
-                              'G' -> 7
-                              'T' -> 8
-
-quantifyEmbarked :: String -> Float
-quantifyEmbarked "" = 0
-quantifyEmbarked "C" = 1
-quantifyEmbarked "Q" = 2
-quantifyEmbarked "S" = 3
 
